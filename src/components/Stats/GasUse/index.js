db0a60cd064bf86305b3ff52cd8d1a08a04cf362
Fabio Hildebrand <polaco@Fabios-MacBook-Pro.local>
2018-03-28 00:52:23 -0300

    gas usage graph time based

--- src/components/Stats/GasUse/index.js
+++ src/components/Stats/GasUse/index.js
@@ -30,7 +30,7 @@ class GasUse extends Component {
       var chartData = []
       _.each(data, (value, index) => {
         chartData.push({
-            block: data[index].number,
+            block: data[index].timestamp,
             a: data[index].gasLimit - data[index].gasUsed,
             b: data[index].gasUsed
         })
@@ -45,7 +45,7 @@ class GasUse extends Component {
                 <ResponsiveContainer width="100%" height={58}>
                 <AreaChart width={600} height={400} data={chartData} stackOffset="expand" margin={{top: 0, right: 0, left: 0, bottom: 0}} >
                 <Tooltip cursor={false} wrapperStyle={{ background: "#303030", border: 0}} coordinate={{ x: 100, y: 140 }} offset={50} />
-                <XAxis dataKey="block" tickCount={0} tickSize={0} tick={false} axisLine={false} hide={true}/>
+                <XAxis dataKey="block" type="number" domain={['auto', 'auto']} tickCount={0} tickSize={0} tick={false} axisLine={false} hide={true}/>
                 <Area type='monotone' dataKey='b' name="Gas Used" stackId="1" stroke='#E74C3C' fill='#E74C3C' />
                 <Area type='monotone' dataKey='a' name="Unused Gas Limit" stackId="1" stroke='#00bc8c' fill='#00bc8c' />
                 </AreaChart>
