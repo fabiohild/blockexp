1f64ef10e5be4e7259548f8a2891c33903f9d4f7
fabiohild <fabiohildebrand@gmail.com>
2018-04-06 02:54:33 -0300

    github corner

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react'
 import './style.css'
 import Stats from './../Stats';
+import GithubCorner from 'react-github-corner';
 
 class Home extends Component {
 
   render() {
       return (
-          <div className="Home container">
-              <Stats/>
-          </div>
+          <div>
+            <div className="Home container">
+                <Stats/>
+            </div>
+            <div>
+                <GithubCorner href="https://github.com/fabiohild/blockexp" octoColor="#303030" bannerColor="#00bc8c" direction="left" />
+            </div>
+        </div>
       );
   }
 }
